41cd01779ba0b56649db0e6fae6a5cbd12fae94d
wevertonDV <weverton.antonio@escolar.ifrn.edu.br>
2024-11-08 16:24:35 -0300

    andando

--- parsers 01/cardapio_parsers.py
+++ parsers 01/cardapio_parsers.py
@@ -1,16 +1,35 @@
 from xml.dom.minidom import parse
+
 dom = parse("cardapio.xml")
+
 cardapio = dom.documentElement
+
 pratos = cardapio.getElementsByTagName('prato')
+
 for prato in pratos:
     nome = prato.getAttribute(id)
-    descricao = descricao.getElementsByTagName('descricao')[0]
+    nome = prato.getElementsByTagName('nome')[0]
+    nome = nome.firstChild.nodeValue
+    
+    descricao = prato.getElementsByTagName('descricao')[0]
     descricao = descricao.firstChild.nodeValue
+    
+    ingredientes_do_pratos = prato.getElementsByTagName('ingredientes')[0]
+    ingredientes_do_pratos = prato.firstChild.nodeValue
 
-    ingredientes_do_pratos = ingredientes.getElementsByTagName('ingredientes')[0]
-    origem = elemento_autor.getAttribute('origem')
-    autor = elemento_autor.firstChild.nodeValue
-    elemento_ano = livro.getElementsByTagName('ano')[0]
-    ano = elemento_ano.firstChild.nodeValue
+    preco = prato.getAttribute('preco')
+
+    calorias = prato.getAttribute('calorias')[0]
+    calorias = calorias.firstChild.nodeValue
+
+    tempoPreparo = prato.getAttribute('tempo de preparo')
+    tempoPreparo = prato.firstChild.nodeValue
 
     print("---\n")
+
+    print ("nome do prato:",nome)
+    print ("descrição:",descricao)
+    print ("ingredientes:",ingredientes_do_pratos)
+    print ("preço:",preco)
+    print ("calorias:",calorias)
+    print ("tempo de peparo:",tempoPreparo)
